87c640fd94cb3d44bad9ef42fd6b1fb2d9a7f708
verma <verma.mayank,97@gmail.com>
2022-01-11 23:24:48 +0530

    initial commit

---
+++ CSVDataLoaderDemo/CSVDataLoaderDemo/src/main/java/com/mayank/CSVDataLoaderDemo/CsvDataLoaderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.CSVDataLoaderDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvDataLoaderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvDataLoaderDemoApplication.class, args);
+	}
+
+}
---
+++ CSVDataLoaderDemo/CSVDataLoaderDemo/src/test/java/com/mayank/CSVDataLoaderDemo/CsvDataLoaderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.CSVDataLoaderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvDataLoaderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
