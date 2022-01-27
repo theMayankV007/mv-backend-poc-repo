693e6a8abaf0f9cf1ebc126fe4f623c863ef63cf
verma <verma.mayank,97@gmail.com>
2022-01-27 23:59:11 +0530

    Initial Commit for reactive web service POC

---
+++ ReactiveCrudService1/src/main/java/reactive/crud/WebAppTwo/WebAppTwoApplication.java
@@ -0,0 +1,13 @@
+package reactive.crud.WebAppTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppTwoApplication.class, args);
+	}
+
+}
